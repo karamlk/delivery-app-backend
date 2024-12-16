9958276d220e463574c23009b4de7052628d01a2
Kar <karam.alrifai24@gmail.com>
2024-12-16 19:02:07 +0300

    fix fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'description','stock'];
+    protected $fillable = ['name', 'price', 'description','stock','image_url'];
 
     public function store()
     {
